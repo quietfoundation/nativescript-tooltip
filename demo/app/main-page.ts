9bc5e67a9291fab16aa6a1b106e2f3cad9ffb5a8
Hoang <hoang.c.nguyen@gmail.com>
2018-06-15 11:10:26 -0500

    Got demo app working

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,27 +1,27 @@
-import * as observable from 'data/observable';
-import * as pages from 'ui/page';
-import {HelloWorldModel} from './main-view-model';
-import {ToolTip} from "nativescript-tooltip";
-import {EventData} from "data/observable";
-import * as app from "application";
-import {TextView} from "ui/text-view";
-import {Color} from "color";
+import * as observable from 'tns-core-modules/data/observable';
+import * as pages from 'tns-core-modules/ui/page';
+import { HelloWorldModel } from './main-view-model';
+import { ToolTip } from 'nativescript-tooltip';
+import { EventData } from 'tns-core-modules/data/observable';
+import * as app from 'tns-core-modules/application';
+import { TextView } from 'tns-core-modules/ui/text-view';
+import { Color } from 'tns-core-modules/color';
 // Event handler for Page 'loaded' event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
-    // Get the event sender
-    let page = <pages.Page>args.object;
-    page.bindingContext = new HelloWorldModel();
+  // Get the event sender
+  let page = <pages.Page>args.object;
+  page.bindingContext = new HelloWorldModel();
 }
 
 export function pushIt(args) {
-   const t = new ToolTip(args.object, {
-        text: "Testing le Tester",
-        position: "bottom",
-        hideArrow: false,
-        textColor: "white",
-        backgroundColor: "blue",
-        style: "CustomToolTipLayoutStyle",
-        width:400
-    });
-    t.show();
+  const t = new ToolTip(args.object, {
+    text: 'Testing le Tester',
+    position: 'bottom',
+    hideArrow: false,
+    textColor: 'white',
+    backgroundColor: 'blue',
+    style: 'CustomToolTipLayoutStyle',
+    width: 400
+  });
+  t.show();
 }
--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,2 @@
-export * from './tooltip.ios';
-
 // Export any shared classes, constants, etc.
 export * from './tooltip.common';
